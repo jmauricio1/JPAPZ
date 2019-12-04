0e7473ceeb77e34c919d1fa723d5e7933f61e5d0
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2019-12-04 13:56:39 -0800

    Putting my new project into the master. This project uses Space X's public API.

---
+++ JOSH/For-Fun/LaunchMe/Scripts/main.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "api/launches",
+        success: displayLaunches,
+        error: errorOnAjax
+    });
+});
+
+function displayLaunches(data) {
+    console.log(data);
+}
+
+function errorOnAjax() {
+    console.log("ERROR on ajax request");
+}
