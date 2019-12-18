aec815c3402dbcf7f092f9ce786793ad326e47df
Joshua Jacob Mauricio (Laptop) <jmauricio15@wou.edu>
2019-12-18 02:51:33 -0800

    Started to implement a better design of the LaunchMe app

---
+++ JOSH/For-Fun/LaunchMe/Controllers/LaunchListController.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaunchMe.Controllers
+{
+    public class LaunchListController : Controller
+    {
+        // GET: LaunchList
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public class Launch
+        {
+            //Launch Information
+            public string missionName;
+            public string launchSuccess;
+            public int flightNum;
+            public string launchTime;
+            public int launchYear;
+
+            //Rocket Information
+            public string rocketName;
+            public string rocketType;
+            public string rocketID;
+            public string launchDetails;
+        }
+        public JsonResult ViewList()
+        {
+            string json = SendRequest("https://api.spacexdata.com/v3/launches");
+
+            JArray data = JArray.Parse(json);
+            List<Launch> launches = new List<Launch>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                Launch launch = new Launch();
+
+                launch.missionName = (string)data[i]["mission_name"];
+                launch.flightNum = (int)data[i]["flight_number"];
+                launch.launchSuccess = (string)data[i]["launch_success"];
+                launch.launchYear = (int)data[i]["launch_year"];
+
+                launches.Add(launch);
+            }
+
+            return Json(launches, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ViewLaunches()
+        {
+            string json = SendRequest("https://api.spacexdata.com/v3/launches");
+
+            JArray data = JArray.Parse(json);
+            List<Launch> launches = new List<Launch>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                Launch launch = new Launch();
+
+                launch.missionName = (string)data[i]["mission_name"];
+                launch.launchSuccess = (string)data[i]["launch_success"];
+                launch.flightNum = (int)data[i]["flight_number"];
+                launch.launchTime = (string)data[i]["launch_date_utc"];
+                launch.launchYear = (int)data[i]["launch_year"];
+
+                launch.rocketName = (string)data[i]["rocket"]["rocket_name"];
+                launch.rocketType = (string)data[i]["rocket"]["rocket_type"];
+                launch.rocketID = (string)data[i]["rocket"]["rocket_id"];
+                launch.launchDetails = (string)data[i]["details"];
+
+                launches.Add(launch);
+            }
+
+            return Json(launches, JsonRequestBehavior.AllowGet);
+        }
+
+        private string SendRequest(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            /*            request.Headers.Add("Authorization", "token " + credentials);
+                        request.UserAgent = username;       // Required, see: https://developer.github.com/v3/#user-agent-required
+                        request.Accept = "application/json";*/
+
+            string jsonString = null;
+            // TODO: You should handle exceptions here
+            using (WebResponse response = request.GetResponse())
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                jsonString = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+            }
+            return jsonString;
+        }
+    }
+}
--- JOSH/For-Fun/LaunchMe/Scripts/main.js
+++ JOSH/For-Fun/LaunchMe/Scripts/main.js
@@ -2,11 +2,68 @@
     $.ajax({
         type: "GET",
         dataType: "json",
+        url: "/api/launchlist",
+        success: displayList,
+        error: errorOnAjax
+    });
+});
+
+/*$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
         url: "/api/launches",
         success: displayLaunches,
         error: errorOnAjax
     });
-});
+});*/
+
+function displayList(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var row = document.createElement('div');
+        row.className = "row";
+
+        var section = document.createElement('div');
+        section.id = "launch-section";
+        section.className = "col-lg-4 col-md-6 col-sm-6 col-xs-12";
+
+        var missname = document.createElement('h2');
+        var missText = document.createTextNode(data[i]["missionName"]);
+        missname.append(missText);
+        missname.className = "section-name";
+
+        var success = document.createElement('h4');
+        if (data[i]["launchSuccess"].toString() == "True") {
+            success.append("Successful");
+            success.className = "success-text";
+        }
+        else {
+            success.append("Unsuccessful");
+            success.className = "fail-text";
+        }
+
+        var missyear = document.createElement('p');
+        var yearText = document.createTextNode(data[i]["launchYear"]);
+        missyear.append(yearText);
+
+        var fnum = document.createElement('p');
+        var launchText = document.createTextNode(data[i]["flightNum"]);
+        //fnum.append("Launch Number: " + data[i]["flight_number"]);fnum.appendChild(launchText);
+        fnum.appendChild(launchText);
+
+        section.appendChild(missname);
+        section.appendChild(success);
+        section.appendChild(missyear);
+        section.appendChild(fnum);
+
+        var otherDiv = document.createElement('div');
+        otherDiv.appendChild(section);
+        /*row.append(missname);
+        row.append(fnum);*/
+        $('#launch-list').append(otherDiv);
+    }
+}
 
 function displayLaunches2(data) {
     console.log(data);
