7d447f566a8256c93b2d3c22726a07f9d0ea62a0
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-03-24 19:08:20 -0700

    making some changes to the Firends Project

--- FriendsProject/Controllers/FriendsController.cs
+++ FriendsProject/Controllers/FriendsController.cs
@@ -23,5 +23,10 @@ namespace FriendsProject.Controllers
         {
             return View();
         }
+
+        public ActionResult Josh()
+        {
+            return View();
+        }
     }
 }
