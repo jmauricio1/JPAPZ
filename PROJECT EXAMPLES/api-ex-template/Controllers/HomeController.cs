d2750a85fc50be29fead4eae9f89aab0d688a0a7
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-07-29 18:27:12 -0700

    creating template for the api example

---
+++ PROJECT EXAMPLES/api-ex-template/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace api_ex_template.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private string SendRequest(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            /*            request.Headers.Add("Authorization", "token " + credentials);
+                        request.UserAgent = username;       // Required, see: https://developer.github.com/v3/#user-agent-required
+                        request.Accept = "application/json";*/
+
+            string jsonString = null;
+            // TODO: You should handle exceptions here
+            using (WebResponse response = request.GetResponse())
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                jsonString = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+            }
+            return jsonString;
+        }
+    }
+}
