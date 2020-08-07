37b723632d72f84e3ba3e8021cfa9aedccf3c298
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-08-07 13:15:41 -0700

    Initial bulk commit for practice-proj

---
+++ practice-proj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practice_proj.Startup))]
+namespace practice_proj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
