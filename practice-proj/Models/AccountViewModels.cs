f7afb6ea6b4c71e79d29a307d1c3ee9b33f2a8ac
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-08-12 17:19:10 -0700

    Creating the NavBar and initial Styles

--- practice-proj/Models/AccountViewModels.cs
+++ practice-proj/Models/AccountViewModels.cs
@@ -75,6 +75,10 @@ namespace practice_proj.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
--- practice-proj/Models/IdentityModels.cs
+++ practice-proj/Models/IdentityModels.cs
@@ -9,6 +9,7 @@ namespace practice_proj.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string UN { get; internal set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -21,7 +22,7 @@ namespace practice_proj.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("JPAPZAzure", throwIfV1Schema: false)
         {
         }
 
