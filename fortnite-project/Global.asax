a83cd9718478d27eadce6d9e0e829d2a6c27cab4
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2020-08-14 17:44:10 -0700

    Initial bulk for fortnite project

---
+++ fortnite-project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fortnite_project.MvcApplication" Language="C#" %>
---
+++ fortnite-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fortnite_project.Startup))]
+namespace fortnite_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
